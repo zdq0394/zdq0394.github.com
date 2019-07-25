3cab6e5f0259982ff545112e1540b2f4a27433e7
zdq0394 <zdq123.hn@163.com>
2019-07-25 09:50:12 +0800

    Update local volume preparement scripts

--- kubernetes/storage/localvolume/prepare_disks.sh
+++ kubernetes/storage/localvolume/prepare_disks.sh
@@ -1,19 +1,21 @@
 #! /bin/bash
-mkfs.ext4 /dev/sdb
+DISK="/dev/sdb"
+DISK_NUM="100"
+
+mkfs.ext4 $DISK
+DISK_UUID=`blkid -s UUID -o value $DISK`
+mkdir /mnt/$DISK_UUID
+mount -t ext4 $DISK /mnt/$DISK_UUID
+
+echo UUID=$DISK_UUID /mnt/$DISK_UUID ext4 defaults 0 2 | tee -a /etc/fstab
+
 
 mkdir -p /mnt/fast-disks
-
-DISK_UUID=$(blkid -s UUID -o value /dev/sdb)
-mkdir /mnt/$DISK_UUID
-mount -t ext4 /dev/sdb /mnt/$DISK_UUID
-
-echo UUID=`sudo blkid -s UUID -o value /dev/sdb` /mnt/$DISK_UUID ext4 defaults 0 2 | tee -a /etc/fstab
-
-for i in $(seq 1 100); do
+for i in $(seq 1 $DISK_NUM); do
   mkdir -p /mnt/${DISK_UUID}/vol${i} /mnt/fast-disks/${DISK_UUID}_vol${i}
   mount --bind /mnt/${DISK_UUID}/vol${i} /mnt/fast-disks/${DISK_UUID}_vol${i}
 done
 
-for i in $(seq 1 100); do
+for i in $(seq 1 $DISK_NUM); do
   echo /mnt/${DISK_UUID}/vol${i} /mnt/fast-disks/${DISK_UUID}_vol${i} none bind 0 0 | tee -a /etc/fstab
 done
